d1623db6d3573dc100ba21da44f8d85e04c0afcd
harveybia <harvey.shi@yahoo.com>
2019-03-08 04:53:18 -0500

    ros node for vehicle control

---
+++ ros/src/mky_vctrl/src/chassis_ctrl_node.cpp
@@ -0,0 +1,55 @@
+/**
+ * @file chassis_ctrl.cpp
+ * @author Haowen Shi
+ * @date 7 Mar 2019
+ * @brief ROS wrapper for RoboMonkey vehicle control.
+ */
+
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <pthread.h>
+#include "mkycom.h"
+
+#define MSG_QUEUE_SIZE (1)
+
+int16_t lwheel_vtarget = 0;
+int16_t rwheel_vtarget = 0;
+
+void print_usage(void) {
+  printf("usage: rosrun mky_vctrl chassis_ctrl <tty_path>\n");
+}
+
+void lwheel_vtarget_callback(geometry_msgs::Twist vtarget) {
+    lwheel_vtarget = vtarget.linear.x + vtarget.angular.z;
+    rwheel_vtarget = vtarget.linear.x - vtarget.angular.z;
+
+    mkycom_send_chassis_command(lwheel_vtarget, rwheel_vtarget);
+}
+
+int main(int argc, char **argv) {
+    int err;
+    pthread_t comm_thread;
+
+    if (argc < 2) {
+        print_usage();
+        return -1;
+    }
+
+    if (mkycom_init(argv[1]) < 0) {
+        printf("[failed]: mkycom_init()\n");
+        return -2;
+    }
+
+    // create and start feedback decoding thread
+    pthread_create(&comm_thread, NULL, mkycom_recv_thread, NULL);
+
+    ros::init(argc, argv, "chassis_ctrl");
+
+    ros::NodeHandle node;
+
+    ros::Subscriber sub = node.subscribe("mky_lwheel_vtarget", MSG_QUEUE_SIZE, lwheel_vtarget_callback);
+
+    ros::spin();
+
+    return 0;
+}
