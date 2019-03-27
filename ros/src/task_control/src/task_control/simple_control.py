88411b8fa5070738799ce27fa189d1175b9f90d7
Tiffany <chiangt1tiff@gmail.com>
2019-03-27 18:03:22 -0400

    tested apriltag task control

--- ros/src/task_control/src/task_control/simple_control.py
+++ ros/src/task_control/src/task_control/simple_control.py
@@ -17,6 +17,21 @@ class SimpleControl:
         debug_pub = rospy.Publisher("task_control/debug", String, queue_size=10)
         rospy.Subscriber("tag_detections", AprilTagDetectionArray, callback=self.on_apriltag)
         r = rospy.Rate(4)
+
+
+        # set up parameters
+        self.max_dist = int(rospy.get_param('max_dist', 6))
+        self.max_lin_vel = int(rospy.get_param('max_lin_vel', 1500))
+        self.max_ang_vel = int(rospy.get_param('max_ang_vel', 1500))
+        self.runner_dist = int(rospy.get_param('runner_dist', 2))
+        self.lin_p_val = int(rospy.get_param('lin_p_val', 1000))
+        self.lin_i_val = int(rospy.get_param('lin_i_val', 0))
+        self.lin_d_val = int(rospy.get_param('lin_d_val', 0))
+        self.ang_p_val = int(rospy.get_param('ang_p_val', 1000))
+        self.ang_i_val = int(rospy.get_param('ang_i_val', 0))
+        self.ang_d_val = int(rospy.get_param('ang_d_val', 0))
+
+
         while not rospy.is_shutdown():
             cmd_pub.publish(self.cmd_vel)
             debug_pub.publish(self.debug_msg)
@@ -29,9 +44,12 @@ class SimpleControl:
             self.pid_control()
         else:
             self.debug_msg = "no tag"
+            self.cmd_vel.linear.x = 0
+            self.cmd_vel.linear.y = 0
+            self.cmd_vel.angular.z = 0
 
     def pid_control(self):
-        dist = math.hypot(self.pose.position.x, self.pose.position.y)
+        dist = math.sqrt((self.pose.position.x ** 2) + (self.pose.position.y ** 2) + (self.pose.position.z **2))
         rot = self.pose.orientation
         (roll, pitch, yaw) = euler_from_quaternion([rot.x, rot.y, rot.z, rot.w])
         if roll < 0:
@@ -43,17 +61,25 @@ class SimpleControl:
         # pitch: up/down rotation about camera
         # yaw: camera rotation
 
-        lin_pid = PID(1000, 0, 0)
-        ang_pid = PID(1, 0, 0)
-	lin_pid.setpoint = 2
-        lin_vel = lin_pid(dist)
-        ang_vel = ang_pid(theta)
+        lin_pid = PID(self.lin_p_val, self.lin_i_val, self.lin_d_val)
+        ang_pid = PID(self.ang_p_val, self.ang_i_val, self.ang_d_val)
+        lin_pid.setpoint = self.runner_dist
+        lin_vel = - lin_pid(dist)
+        ang_vel = ang_pid(self.pose.position.x)
+        if lin_vel > self.max_lin_vel:
+            lin_vel = self.max_lin_vel
+        elif lin_vel < -self.max_lin_vel:
+            lin_vel = -self.max_lin_vel
+        if ang_vel > self.max_ang_vel:
+            ang_vel = self.max_ang_vel
+        elif ang_vel < -self.max_ang_vel:
+            ang_vel = -self.max_ang_vel
 	
 	# velocity: current value
-        self.cmd_vel.linear.x = - lin_vel
+        self.cmd_vel.linear.x = lin_vel
         self.cmd_vel.linear.y = 0
-	self.cmd_vel.angular.x = dist
-	self.cmd_vel.angular.y = theta
+        self.cmd_vel.angular.x = dist
+        self.cmd_vel.angular.y = theta
         self.cmd_vel.angular.z = ang_vel
 
     def naive_control(self):
